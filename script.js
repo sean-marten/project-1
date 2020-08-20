573f1a4f48eefe02b91c53f0bfd40d95c44198ec
Lindsey Smith <campbelllm@gmail.com>
2020-08-19 18:37:32 -0600

    added api call for bio and movie images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    const NYTAPI = 'Vzp9bxJBwPxOUom0nJ0iuroOmy8wXd05';
-    const NYTMovie = '';//we will need to grab the movie name from wherever it will be rendered on the page
-    const NTYQueryMovies = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query='+ NYTMovie + 'api-key=' + NYTAPI;
+    const movieGluAPI = '5zwPt8bzCu7r7rifnKoCd827XemsTLcO6rk51vTy';
+    // const NYTMovie = '';//we will need to grab the movie name from wherever it will be rendered on the page
+    // const NTYQueryMovies = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query='+ NYTMovie + 'api-key=' + NYTAPI;
     const TMDBAPI = '0adfd846cf8f1168484b5da4e5339d7c';
     const personSearch = 'matthew mcconaughey' ;//function around get requests that gets value of input when search is clicked
     const TMDBPersonID ='https://api.themoviedb.org/3/search/person?api_key=' + TMDBAPI + '&language=en-US&query=' + personSearch;
@@ -23,6 +23,7 @@ $(document).ready(function(){
         $.get(TMDBPersonID, function(response){
             const personID = response.results[0].id
             getMovieData(personID)
+            getBioData(personID)
         })
         //uses that person ID to do an in depth search for movies
         function getMovieData(personID){
@@ -32,11 +33,32 @@ $(document).ready(function(){
                 const sortRating = movieArray.sort((a,b) => {
                     return b.vote_average - a.vote_average
                 }).slice(0,3)
-        
+                //looping through the sorted movies, getting movie poster image and appending to body for now
+                $.each(sortRating, function(index){
+                    const movieImage = $('<img>').attr('src', 'https://image.tmdb.org/t/p/w500/' + sortRating[index].poster_path);
+                    // $('body').append(movieImage)
+                })
+                
                 console.log("SORTED___", sortRating);
             })
         }
+        function getBioData(personID){
+            const bioData = 'https://api.themoviedb.org/3/person/'+ personID + '?api_key=' + TMDBAPI ;
+            $.get(bioData, function(response){
+                console.log(response)
+                //grabbing info for bio section, we can uncomment once we link to the HTML elements, Still need to figure out the picture
+                // const actorName = $('').text(response.name)
+                // const actorBirthday = $('').text(response.birthday)
+                // const actorKnownFor = $('').text(response.known_for_department)
+                // const actorBirthplace = $('').text(response.place_of_birth)
+
+                // the 'w' in the url below indicates the size of the image
+                const actorImage = $('<img>').attr('src', 'https://image.tmdb.org/t/p/w500/' + response.profile_path)
+                // $('body').append(actorImage)
+            })
+        }
     }
+   retreiveData();
    
     
 })
