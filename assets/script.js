211a577366aad6a5f30b83d6eedcbbebfc402ae3
Lindsey Smith <campbelllm@gmail.com>
2020-08-21 14:29:58 -0600

    created cards dynamically

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,10 @@ $(document).ready(function () {
   let personSearch = "";
 
   $(document).on("click", ".search", function () {
-    console.log("here");
-
+    $('.topRated').empty()
     personSearch = $(".input").val();
-    console.log(personSearch);
-
     retreiveData();
+    $('.input').val('')
   });
 
   function retreiveData() {
@@ -23,22 +21,31 @@ $(document).ready(function () {
     });
     //uses that person ID to do an in depth search for movies
     function getMovieData(personID) {
-      const tmdbMovies = `https://api.themoviedb.org/3/person/${personID}/combined_credits?api_key=${tmdbApi}&language=en-US`;
+      const tmdbMovies = `https://api.themoviedb.org/3/person/${personID}/movie_credits?api_key=${tmdbApi}&language=en-US`;
 
       $.get(tmdbMovies, function (response) {
         const movieArray = response.cast;
         const sortRating = movieArray
           .sort((a, b) => {
             return b.vote_average - a.vote_average;
-          })
-          .slice(0, 3);
-        //looping through the sorted movies, getting movie poster image and appending to body for now
+          }).slice(0, 3);
+          console.log(sortRating)
+        //looping through the sorted movies, creating cards with info
         $.each(sortRating, function (index) {
-          const movieImage = $("<img>").attr(
-            "src",
-            "https://image.tmdb.org/t/p/w500/" + sortRating[index].poster_path
-          );
-          // $('body').append(movieImage)
+          const cardContainer = $('<div>').addClass('col-lg-4 col-md-6')
+          const createCard = $('<div>').addClass('card')
+          const cardImgContainer = $('<div>').addClass('view')
+          const cardImg =$('<img>').attr("src",
+          "https://image.tmdb.org/t/p/w200/" + sortRating[index].poster_path).attr('alt', 'movie poster').addClass('card-img-top')
+          cardImgContainer.append(cardImg)
+          const cardBody = $('<div>').addClass('card-body elegant-color white-text')
+          const cardTitle = $('<h3>').addClass('cardTitle').text(sortRating[index].title)
+          const cardText = $('<p>').addClass('card-text white-text').text(sortRating[index].overview)
+          const cardRating = $('<p>').addClass('card-text white-text').text('Rating: ' + sortRating[index].vote_average + '/10')
+          cardBody.append(cardTitle, cardText, cardRating)
+          createCard.append(cardImgContainer, cardBody)
+          cardContainer.append(createCard)
+          $('.topRated').append(cardContainer)
         });
       });
     }
@@ -60,4 +67,5 @@ $(document).ready(function () {
       });
     }
   }
+  
 });
